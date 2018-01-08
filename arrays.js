0f46e548e4165ba1c8ca768793bab6c5c77d7c85
Jessie Chen <jessiechen0513@gmail.com>
2018-01-08 17:14:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
-// var ingredient1 =
-// "bread"
-// var ingredient2 =
-// "mild cheese"
-// var ingredient3 =
-// "sharp cheese"
-// var ingredient4 =
-// "butter"
-// var ingredient5 =
-// "tomato"
-// var ingredient1 =
-// "garlic"
+var ingredient1 =
+"bread"
+var ingredient2 =
+"mild cheese"
+var ingredient3 =
+"sharp cheese"
+var ingredient4 =
+"butter"
+var ingredient5 =
+"tomato"
+var ingredient1 =
+"garlic"
