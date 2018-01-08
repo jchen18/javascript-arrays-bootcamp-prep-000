25489a2166512b9ae1f466cdf49b9baf6d99bf9b
Jessie Chen <jessiechen0513@gmail.com>
2018-01-08 17:36:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -58,3 +58,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray (array, index){
   return (array[index])
 }
+
+function destructivelyRemoveElementFromBeginningOfArray (array){
+  array.unshift(array[0])
+}
