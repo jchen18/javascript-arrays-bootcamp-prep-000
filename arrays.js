299dcdf19236e363a710cde275164d49eda1da58
Jessie Chen <jessiechen0513@gmail.com>
2018-01-08 17:25:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,6 +41,5 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array = [element,...array]
-  return array
+  array.unshift(element)
 }
